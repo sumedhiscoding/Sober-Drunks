021cd76ed9fa8d46a1d9c0c43dc8c450d22f0634
sumedh <sumedhiscoding@gmail.com>
2022-08-27 16:42:41 +0530

    api called

--- Travel Advisor/src/App.jsx
+++ Travel Advisor/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header/Header";
 import List from "./List/List";
 import PlaceDetails from "./PlaceDetails/PlaceDetails";
 import Map from "./Map/Map";
 import "./App.css";
 import { CssBaseline, Grid } from "@mui/material";
-
+import { getPlacesData } from "../src/api/Index";
 function App() {
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, []);
+  const [places, setPlaces] = useState([]);
+
   return (
     <>
       <CssBaseline />
